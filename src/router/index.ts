29b4cd6f9906bad14752b475ba2d16ba4faa9360
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-06-03 15:25:49 +0900

    feat: :sparkles: TS 설정 및 Home 페이지 제작
    Ts를 위한 tsconfig 파일 설정과 Home 페이지 제작

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import {createRouter, createWebHistory} from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+import Home from "../pages/Home.vue";
+
+
+const routes : RouteRecordRaw[] = [
+    { path: "/" ,name:"Home", component: Home},
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router
