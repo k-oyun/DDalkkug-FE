523965e83f097cc9486861f8ae0bdf651e933bdd
choosla <cnckddn0146@gmail.com>
2025-06-27 02:46:47 +0900

    feat(MySpace): :sparkles: 나의 공간 완성
    - 나의 공간에서 마이그룹과 마이페이지를 통합
    - 마이그룹 API 연동
    - 마이그룹 더미데이터 삭제

--- src/components/BaseNavItem.vue
+++ src/components/BaseNavItem.vue
@@ -4,6 +4,7 @@
       'flex cursor-pointer items-center space-x-2 rounded px-2 py-2 text-lg',
       active ? 'text-cyan-400' : 'text-white hover:text-cyan-300',
     ]"
+    @click="emit('click')"
   >
     <span><slot name="icon" /></span>
     <span><slot name="text" /></span>
@@ -14,6 +15,7 @@
 defineProps({
   active: Boolean,
 });
+const emit = defineEmits(["click"]);
 </script>
 
 <style scoped></style>
--- src/components/MyGroup.vue
+++ src/components/MyGroup.vue
@@ -4,72 +4,74 @@
 
   <!-- 그룹생성 모달창 -->
   <BaseGroupCreateModal />
-  <!-- 탭 메뉴 -->
-  <div class="text-md mb-3 flex justify-center md:justify-start md:space-x-4">
-    <BaseButton
-      neonColor="#00ccff"
-      class="md:text-md text-xs"
-      :style="{ color: buttonState == 0 ? '#00ccff' : '#d9d9d9' }"
-      @click="change(0)"
-    >
-      가입한 그룹
-    </BaseButton>
-    <BaseButton
-      neonColor="#00ccff"
-      class="md:text-md text-xs"
-      :style="{ color: buttonState == 1 ? '#00ccff' : '#d9d9d9' }"
-      @click="change(1)"
-    >
-      그룹 가입
-    </BaseButton>
-    <BaseButton
-      neonColor="#00ccff"
-      class="md:text-md text-xs"
-      :style="{ color: buttonState == 2 ? '#00ccff' : '#d9d9d9' }"
-      @click="change(2)"
-    >
-      나의 그룹 관리
-    </BaseButton>
-  </div>
-
-  <div
-    class="neon-border rounded-full bg-white"
-    style="--neon-color: #ffffff; border: 1px solid"
-  ></div>
-
-  <div class="mt-5 space-y-6">
-    <div v-if="!loading">
-      <div
-        class="grid max-w-full grid-cols-1 items-center gap-6 transition-[max-width] duration-200 ease-in-out md:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3"
+  <div class="overflow-y-auto p-3 md:p-6">
+    <!-- 탭 메뉴 -->
+    <div class="text-md mb-3 flex justify-center md:justify-start md:space-x-4">
+      <BaseButton
+        neonColor="#00ccff"
+        class="md:text-md text-xs"
+        :style="{ color: buttonState == 0 ? '#00ccff' : '#d9d9d9' }"
+        @click="change(0)"
       >
-        <div v-for="group in groupDatas" :key="group.id">
-          <BaseGroupCard
-            :groupId="group.id"
-            :leaderId="group.leaderId"
-            :name="group.name"
-            :description="group.description"
-            :totalPaid="group.totalPaid"
-            memberCount="3"
-          />
+        가입한 그룹
+      </BaseButton>
+      <BaseButton
+        neonColor="#00ccff"
+        class="md:text-md text-xs"
+        :style="{ color: buttonState == 1 ? '#00ccff' : '#d9d9d9' }"
+        @click="change(1)"
+      >
+        그룹 가입
+      </BaseButton>
+      <BaseButton
+        neonColor="#00ccff"
+        class="md:text-md text-xs"
+        :style="{ color: buttonState == 2 ? '#00ccff' : '#d9d9d9' }"
+        @click="change(2)"
+      >
+        나의 그룹 관리
+      </BaseButton>
+    </div>
+
+    <div
+      class="neon-border rounded-full bg-white"
+      style="--neon-color: #ffffff; border: 1px solid"
+    ></div>
+
+    <div class="mt-5 space-y-6">
+      <div v-if="!loading">
+        <div
+          class="grid max-w-full grid-cols-1 items-center gap-6 transition-[max-width] duration-200 ease-in-out md:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3"
+        >
+          <div v-for="group in groupDatas" :key="group.id">
+            <BaseGroupCard
+              :groupId="group.id"
+              :leaderId="group.leaderId"
+              :name="group.name"
+              :description="group.description"
+              :totalPaid="group.totalPaid"
+              memberCount="3"
+            />
+          </div>
         </div>
       </div>
+      <div v-else class="flex w-full justify-center pt-10 text-white">
+        <IconLineMdLoadingTwotoneLoop class="text-[100px] text-[#00ccff]" />
+      </div>
     </div>
-    <div v-else class="flex w-full justify-center pt-10 text-white">
-      <IconLineMdLoadingTwotoneLoop class="text-[100px] text-[#00ccff]" />
+
+    <!-- <div class="fixed bottom-5 left-5">
+            <IconMaterialSymbolsVerifiedOutlineRounded
+              class="text-2xl opacity-60"
+            />
+          </div> -->
+
+    <div v-if="buttonState == 2" class="fixed right-5 bottom-5 text-white">
+      <IconMdiPlusCircle
+        class="h-15 w-15 opacity-50"
+        @click="() => groupCreateModalStore.setIsOpen(true)"
+      ></IconMdiPlusCircle>
     </div>
-  </div>
-
-  <!-- <div class="fixed bottom-5 left-5">
-          <IconMaterialSymbolsVerifiedOutlineRounded
-            class="text-2xl opacity-60"
-          />
-        </div> -->
-
-  <div v-if="buttonState == 2" class="fixed right-5 bottom-5 text-white">
-    <IconMdiPlusCircle
-      class="h-15 w-15 opacity-50"
-      @click="() => groupCreateModalStore.setIsOpen(true)"
-    ></IconMdiPlusCircle>
   </div>
 </template>
 
